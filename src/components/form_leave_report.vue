3b078dcb25b8f57e5e85f0df578d312c692f5e48
nokranger <anusorn.thavornporn@gmail.com>
2020-11-04 17:14:18 +0700

    check leave

--- src/components/form_leave_report.vue
+++ src/components/form_leave_report.vue
@@ -4,11 +4,23 @@
         <br>
         <b-row>
           <b-col>
+            <!-- <b-button variant="success" v-on:click="check_leave">ตรวจสอบจำนวนวันลาที่เหลือ</b-button> -->
+          </b-col>
+          <b-col></b-col>
+          <b-col>
+            <div class="align-left">
+              <label style="color:red;font-size:25px;">*</label>  วันลาที่เหลือ
+            </div>
+            <div><b-input type="text" v-model="leaveCount" readonly></b-input></div>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
             <div>
               <div class="align-left">
                <label style="color:red;font-size:25px;">*</label> Start
               </div>
-              <b-form-input type="datetime-local" v-model="leaveActivityReport.start_time"></b-form-input>
+              <b-form-input id="start" type="datetime-local" v-model="leaveActivityReport.start_time"></b-form-input>
             </div>
           </b-col>
           <b-col>
@@ -16,15 +28,21 @@
               <div class="align-left">
               <label style="color:red;font-size:25px;">*</label>  End
               </div>
-              <b-form-input type="datetime-local" v-model="leaveActivityReport.end_time"></b-form-input>
+              <b-form-input  id="end" type="datetime-local" v-model="leaveActivityReport.end_time"></b-form-input>
             </div>
           </b-col>
           <b-col>
-            <div>
+            <!-- <div>
               <div class="align-left">
               <label style="color:red;font-size:25px;">*</label>  Amount
               </div>
               <b-form-input type="text"></b-form-input>
+            </div> -->
+          <div>
+            <div class="align-left">
+              <label style="color:red;font-size:25px;">*</label>  Amount
+            </div>
+              <b-form-input id="amount" type="text"></b-form-input>
             </div>
           </b-col>
         </b-row>
@@ -37,7 +55,7 @@
               <div class="align-left">
               <label style="color:red;font-size:25px;">*</label>  Leave type
               </div>
-              <b-form-select ref="leave_report" v-model="selected" :options="options"></b-form-select>
+              <b-form-select id="type" ref="leave_report" v-model="selected" :options="options"></b-form-select>
               <!-- <b-form-input type="text" v-model="leaveActivityReport.leave_category"></b-form-input> -->
             </div>
           </b-col>
@@ -46,7 +64,7 @@
               <div class="align-left">
               <label style="color:red;font-size:25px;">*</label>  Reason
               </div>
-              <b-form-input type="text" v-model="leaveActivityReport.reason_for_leave"></b-form-input>
+              <b-form-input  id="reason" type="text" v-model="leaveActivityReport.reason_for_leave"></b-form-input>
             </div>
           </b-col>
           <!-- <b-col>
@@ -78,7 +96,7 @@
             <b-col>
               <br>
               <div>
-                <b-button style="width:30%" variant="outline-primary" v-on:click="send ()"><i class="fas fa-upload"></i></b-button>
+                <b-button id="send" style="width:30%" variant="primary" v-on:click="send ()"><i class="fas fa-upload"></i></b-button>
               </div>
             </b-col>
           </b-row>
@@ -104,10 +122,11 @@ export default {
         status: 0,
         file: '/doc/nok.docs'
       },
+      leaveCount: '',
       selected: null,
       options: [
         {
-          value: null, text: 'Please Select'
+          value: null, text: 'Please Select', disabled: true
         },
         {
           value: 1, text: 'ลากิจ'
@@ -121,8 +140,11 @@ export default {
       ]
     }
   },
+  created () {
+  },
   mounted () {
     this.leaveActivityReport.employee_id = JSON.parse(localStorage.getItem('username'))
+    this.check_leave()
   },
   methods: {
     send () {
@@ -153,6 +175,37 @@ export default {
         .catch(e => {
           this.error.push(e)
         })
+    },
+    dis_form () {
+      document.getElementById('send').disabled = true
+      document.getElementById('start').readOnly = true
+      document.getElementById('end').readOnly = true
+      document.getElementById('amount').readOnly = true
+      document.getElementById('type').disabled = true
+      document.getElementById('reason').readOnly = true
+    },
+    en_form () {
+      document.getElementById('send').disabled = false
+      document.getElementById('start').readOnly = false
+      document.getElementById('end').readOnly = false
+      document.getElementById('amount').readOnly = false
+      document.getElementById('type').disabled = false
+      document.getElementById('reason').readOnly = false
+    },
+    check_leave () {
+      console.log('checkleave')
+      const emp = {
+        id: JSON.parse(localStorage.getItem('username'))
+      }
+      axios.post('http://127.0.0.1:4000/leavear/checkleave', emp).then(response => {
+        this.leaveCount = response.data.result[0].leave_activity
+        console.log('res', this.leaveCount)
+        if (this.leaveCount <= 0 || this.leaveCount === '') {
+          this.dis_form()
+        } else if (this.leaveCount > 0 || this.leaveCount !== '') {
+          this.en_form()
+        }
+      })
     }
   }
 }
