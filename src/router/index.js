fee296327478cfba89b3adca7e71380a27f01430
nokranger <anusorn.thavornporn@gmail.com>
2020-11-10 17:31:01 +0700

    ref

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Signup from '../views/Signup.vue'
-import Login from '../views/login.vue'
+// import Login from '../views/login.vue'
 import Dashboard from '../views/dashboard.vue'
 import Approve from '../views/approvePage.vue'
 import Approveleave from '../views/approve_leave.vue'
@@ -27,25 +27,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Login
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
   },
   {
     path: '/signup',
     name: 'signup',
     component: Signup
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
   },
   {
     path: '/dashboard',
@@ -115,14 +102,6 @@ const routes = [
   {
     path: '*',
     redirect: '/'
-  },
-  {
-    path: '/upload',
-    component: () => import(/* webpackChunkName: "about" */ '../views/uploadimage.vue')
-  },
-  {
-    path: '/pdf',
-    component: () => import(/* webpackChunkName: "about" */ '../views/testpdf.vue')
   }
 ]
 
